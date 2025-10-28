da75c6c21e0a00d6ad23c39bfbc1b4629100b684
Vixwcsa <Bogoduhova2007@yandex.ru>
2025-10-28 15:25:20 +0300

    Add files via upload

---
+++ ps.2.9.10..pas
@@ -0,0 +1,39 @@
+﻿program two;
+var
+  arr: array[1..8, 1..8] of integer;
+  res: array[1..8] of integer;
+  i, j, max_val, cnt: integer;
+begin
+  randomize;
+  writeln('Двумерный массив:');
+  for i := 1 to 8 do
+  begin
+    for j := 1 to 8 do
+    begin
+      arr[i, j] := random(50) + 1;
+      write(arr[i, j]:3);
+    end;
+    writeln;
+  end;
+  for i := 1 to 8 do
+  begin
+    max_val := 0;
+    cnt := 0;
+    for j := 1 to 8 do
+      if arr[i, j] > max_val then
+        max_val := arr[i, j];
+    for j := 1 to 8 do
+      if arr[i, j] = max_val then
+        cnt := cnt + 1;
+    if cnt = 1 then res[i] := 1
+    else res[i] := -1;
+  end;
+  writeln;
+  write('Результат: [');
+  for i := 1 to 8 do
+  begin
+    write(res[i]);
+    if i < 8 then write(', ');
+  end;
+  writeln(']');
+end.
---
+++ ps1.9.10..pas
@@ -0,0 +1,64 @@
+﻿program one;
+var
+  x, y, i, sum1, sum2: integer;
+  lst: array[1..100] of integer;
+  tpm: array[1..100] of integer;
+begin
+  randomize;
+  
+  write('Введите размер списка 1: ');
+  readln(x);
+  for i := 1 to x do
+    lst[i] := random(21) - 10;
+  
+  write('Введите размер списка 2: ');
+  readln(y);
+  for i := 1 to y do
+    tpm[i] := random(21) - 10;
+  
+  write('Массив 1: ');
+  for i := 1 to x do
+    write(lst[i], ' ');
+  writeln;
+  
+  write('Массив 2: ');
+  for i := 1 to y do
+    write(tpm[i], ' ');
+  writeln;
+  
+  sum1 := 0;
+  sum2 := 0;
+  
+  for i := 1 to x do
+    if lst[i] > 0 then
+      sum1 := sum1 + lst[i];
+      
+  for i := 1 to y do
+    if tpm[i] > 0 then
+      sum2 := sum2 + tpm[i];
+  
+  if sum1 > sum2 then
+  begin
+    for i := 1 to y do
+      tpm[i] := tpm[i] * 10;
+    writeln('Умножили второй массив');
+  end
+  else if sum1 < sum2 then
+  begin
+    for i := 1 to x do
+      lst[i] := lst[i] * 10;
+    writeln('Умножили первый массив');
+  end
+  else
+    writeln('Суммы равны');
+  
+  write('Массив 1: ');
+  for i := 1 to x do
+    write(lst[i], ' ');
+  writeln;
+  
+  write('Массив 2: ');
+  for i := 1 to y do
+    write(tpm[i], ' ');
+  writeln;
+end.
